1ff4cb3da3a89516f5abd70a49ed34823ad1eca4
Odin Dutton <odindutton@gmail.com>
2018-08-06 20:46:25 +1000

    Implement first few instructions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+#[derive(Debug)]
+struct State {
+    b: u8,
+    pc: u16,
+    sp: u16,
+}
+
+#[allow(dead_code)]
 fn disassemble(pc: usize, buffer: &Vec<u8>) -> usize {
     let mut op_bytes = 1;
     let op_code = buffer[pc];
@@ -21,19 +29,62 @@ fn disassemble(pc: usize, buffer: &Vec<u8>) -> usize {
     op_bytes
 }
 
+fn step(mut state: State, buffer: &Vec<u8>) -> State {
+    let pc = state.pc as usize;
+    let op_code = buffer[pc];
+
+    state.pc += 1;
+
+    match op_code {
+        0x00 => { },
+        0xc3 => {
+            let byte_1 = buffer[pc + 1] as u16;
+            let byte_2 = buffer[pc + 2] as u16;
+
+            state.pc = (byte_2 << 8) | byte_1;
+        },
+        0x31 => {
+            let byte_1 = buffer[pc + 1] as u16;
+            let byte_2 = buffer[pc + 2] as u16;
+
+            state.sp = (byte_2<<8) | byte_1;
+            state.pc += 2;
+        },
+        0x06 => {
+            state.b = buffer[pc + 1];
+            state.pc += 1;
+        },
+        _   => { state.pc = 0 },
+    }
+
+    state
+}
+
 fn main() -> std::io::Result<()> {
     let mut file = File::open("invaders.rom")?;
     let mut buffer = vec![];
+    let mut state = State {
+        b: 0,
+        pc: 0,
+        sp: 0,
+    };
 
     file.read_to_end(&mut buffer)?;
 
-    let mut pc = 0;
+    loop {
+        println!("{:?}", state);
+        disassemble(state.pc as usize, &buffer);
 
-    while pc < buffer.len() {
-        let op_bytes = disassemble(pc, &buffer);
-        if op_bytes == 0 { break }
-        pc += op_bytes;
+        state = step(state, &buffer);
+
+        if state.pc == 0 { break }
     }
+
+    // while pc < buffer.len() {
+    //     let op_bytes = disassemble(pc, &buffer);
+    //     if op_bytes == 0 { break }
+    //     pc += op_bytes;
+    // }
 
     Ok(())
 }
